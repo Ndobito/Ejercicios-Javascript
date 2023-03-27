3db4122e3ca622a358353f8c884aaf5ec1b69183
Ndobito <jcuellarmenza@gmail.com>
2023-03-27 11:54:09 -0500

    Ejercicios Esteban

---
+++ Js/Eje13-Js.js
@@ -0,0 +1,10 @@
+let divs = document.querySelectorAll(".imgs>div");
+
+divs.forEach( e => {
+    e.addEventListener("click", () => {
+        divs.forEach( d =>{
+            d.style.removeProperty("width");
+        })
+        e.style.width = "40%";
+    })
+});
